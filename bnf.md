2f5856507715f25a815c02f135a241a6dce6c7db
Oliver Po <oliver.po2882@gmail.com>
2026-01-12 11:11:00 +1100

    Create lexer

---
+++ bnf.md
@@ -0,0 +1,67 @@
+<expr>  ::= 
+<bool> ::= <bool2> | <bool> "&&" <bool2>
+<bool2> ::= <add-sub> | <bool2> "||" <add-sub>
+<add-sub> ::= <term> | <expr> "+" <term> | <expr> "-" <term>
+<term>     ::= <negative> | <term> "*" <negative> | <term> "/" <negative>
+<negative> ::= "-" <factor>
+<factor>   ::= <number> | "(" <expr> ")" | <function-call> | <identifier>
+<number>   ::= <digit> | <digit> <number>
+<function-call>  ::= <identifier> "(" <expression-list> ")"
+
+<identifier-inner> ::= <letter> | <digit> | <letter> <identifier-inner> | <digit> <identifier-inner>
+<identifier> ::= <letter> <identifier-inner>
+
+<hex-digit> ::= "A" | "B" | "C" | "D" | "E" | "F" | <digit>
+
+<hex> ::= <hex-digit> | <hex-digit> <hex>
+
+<hex-prefix> ::= "0x" <hex>
+
+<octal-digit> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7"
+
+<octal> ::= <octal-digit> | <octal-digit> <octal>
+
+<octal-prefix> ::= "0" <octal>
+
+<digit>    ::= [0-9]
+<letter>   ::= [A-Z] | [a..z]
+<empty>    ::= ""
+
+<primitive> ::= "u0" | "u8" | "u16" | "u32" | "i8" | "i16" | "i32" | "f8" | "f16" | "f32"
+
+<struct> ::= "struct" <identifier> "{" <declaration-list> "}"
+
+<union> ::= "union" <identifier> "{" <declaration-list> "}"
+
+<base-types> ::= <primitive> | <struct> | <union>
+
+<!-- TODO: Handle case of array of pointers -->
+<array> ::= <identifier> | <identifier> "[" <digit> "]" | <array> "[" <digit> "]"
+
+<pointer> ::= <array> | "*" <array> | "*" <pointer> 
+
+<!-- TODO: prevent run time expressions (up to parser?) -->
+<declaration> ::= <base-types> <pointer>
+
+<declaration-list> ::= <declaration> | <declaration-list> "," <declaration>
+
+<declaration-statement> ::= "let" <declaration> | "let" <declaration> "=" <expr> 
+| "let" <pointer> | "let" <pointer> "=" <expr>
+
+<expression-list> ::= <expr> | <expr> "," <expression-list>
+
+<function-def> ::= "fn" <identifier> "->" <type> "(" <declaration-list> ")" <block>
+
+<assignment> ::= <identifier> "=" <expr>
+
+<block>          ::= "{" <statements> "}"
+<statements>     ::= <statement> | <statement> <statements>
+<statement>      ::= <assignment> ";" | <expr> | <return-stmt>
+
+<new-statement> ::= "new" <identifier>
+
+<free-statement> ::= "free" <identifier>
+
+
+
+<return-stmt>    ::= "return" <expression> ";" | <expression> "\n"
---
+++ src/lexer.rs
@@ -0,0 +1,339 @@
+use thiserror::Error;
+
+use std::{
+    collections::VecDeque, fs::File, io::{self, BufReader}, result
+};
+use utf8_chars::BufReadCharsExt;
+
+pub type Result<T> = result::Result<T, LexerError>;
+
+#[derive(Debug, Error)]
+pub enum LexerError {
+    #[error("{0}")]
+    Msg(String),
+
+    #[error(transparent)]
+    Io(#[from] io::Error),
+}
+
+enum Tokens {
+    ASSIGN,
+
+    LOGAND,
+
+    LOGOR,
+
+    BITOR,
+    BITXOR,
+    AMPER, // called amper cause address of uses this symbol as well as bitwise AND
+
+    EQ,
+    NEQ,
+
+    LT,
+    LTE,
+
+    GT,
+    GTE,
+
+    LSHIFT,
+    RSHIFT,
+
+    MINUS, // also unary
+    PLUS,
+
+    ASTERISK, // called asterik cause pointer uses this symbol as well as times
+    DIVIDE,
+
+    MODULO,
+
+    NOT,
+    BITNOT,
+
+    LPARENT, // (
+    RPARENT, // )
+
+    LBRACKET, // [
+    RBRACKET, // ]
+
+    DOT,
+
+    ARROW,
+
+    LBRACE,
+    RBRACE,
+
+    SINGLEQUOTE,
+    STRING(String),
+
+    NUMBER(i32),
+    IDENT(String),
+
+    FN,
+    LET,
+    IF,
+    ELSE,
+    FOR, // no while statement, while is covered by for
+
+    STRUCT,
+    UNION,
+
+    U0, // equivalent to void
+    I8,
+    I16,
+    I32,
+    EOF,
+}
+
+struct Lexer {
+    input_buf: BufReader<File>,
+    putback_buf: VecDeque<char>,
+}
+
+impl Lexer {
+    pub fn new(input_path: String) -> Result<Self> {
+        let file = File::open(input_path)?;
+        Ok(Self {
+            input_buf: BufReader::new(file),
+            putback_buf: VecDeque::new(),
+        })
+    }
+
+    fn consume(&mut self) -> Result<Option<char>> {
+        if let Some(c) = self.putback_buf.pop_front() {
+            Result::Ok(Some(c))
+        } else if let Some(c) = self.input_buf.read_char()? {
+            Result::Ok(Some(c))
+        } else {
+            Result::Ok(None)
+        }
+    }
+
+    fn putback(&mut self, c: char) {
+        self.putback_buf.push_back(c);
+    }
+
+    pub fn scan_token(&mut self) -> Result<Tokens> {
+        while let Some(c) = self.consume()? {
+            if c.is_whitespace() {
+                continue;
+            }
+
+            match c {
+                '&' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '&' {
+                            return Result::Ok(Tokens::LOGAND);
+                        } else {
+                            self.putback(c);
+                            return Result::Ok(Tokens::AMPER);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::AMPER);
+                    }
+                }
+                '|' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '&' {
+                            return Result::Ok(Tokens::LOGOR);
+                        } else {
+                            self.putback(c);
+                            return Result::Ok(Tokens::BITOR);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::BITOR);
+                    }
+                }
+                '=' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '=' {
+                            return Result::Ok(Tokens::EQ);
+                        } else {
+                            self.putback(c);
+                            return Result::Ok(Tokens::ASSIGN);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::ASSIGN);
+                    }
+                }
+
+                '<' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '=' {
+                            return Result::Ok(Tokens::LTE);
+                        } else if c == '<' {
+                            return Result::Ok(Tokens::LSHIFT);
+                        } else  {
+                            self.putback(c);
+                            return Result::Ok(Tokens::LT);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::LT);
+                    }
+                }
+
+                '>' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '=' {
+                            return Result::Ok(Tokens::GTE);
+                        } else if c == '>' {
+                            return Result::Ok(Tokens::RSHIFT);
+                        } else {
+                            self.putback(c);
+                            return Result::Ok(Tokens::GT);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::GT);
+                    }
+                }
+
+                '^' => return Result::Ok(Tokens::BITXOR),
+                '-' => {
+                    if let Some(c) = self.consume()? {
+                        if c == '>' {
+                            return Result::Ok(Tokens::ARROW);
+                        } else {
+                            self.putback(c);
+                            return Result::Ok(Tokens::MINUS);
+                        }
+                    } else {
+                        return Result::Ok(Tokens::MINUS);
+                    }
+                },
+                '+' => return Result::Ok(Tokens::PLUS),
+                '*' => return Result::Ok(Tokens::ASTERISK),
+                '/' => return Result::Ok(Tokens::DIVIDE),
+                '%' => return Result::Ok(Tokens::MODULO),
+                '!' => return Result::Ok(Tokens::NOT),
+                '~' => return Result::Ok(Tokens::BITNOT),
+                '(' => return Result::Ok(Tokens::LPARENT),
+                ')' => return Result::Ok(Tokens::RPARENT),
+                '[' => return Result::Ok(Tokens::LBRACKET),
+                ']' => return Result::Ok(Tokens::RBRACKET),
+                '.' => return Result::Ok(Tokens::DOT),
+                '\'' => return Result::Ok(Tokens::SINGLEQUOTE),
+                '"' => {
+                    let str = self.scan_string()?;
+                    return Result::Ok(Tokens::STRING(str))
+                }
+
+                _ => {
+                    if c.is_numeric() {
+                        self.putback(c); // to be consumed by scan_number
+                        let num = self.scan_number()?;
+                        return Result::Ok(Tokens::NUMBER(num));
+                    } else if c == '{' {
+                        return Result::Ok(Tokens::LBRACE);
+                    } else if c == '}' {
+                        return Result::Ok(Tokens::RBRACE);
+                    } else {
+                        self.putback(c); // to be consumed by keyword
+                        return self.match_keyword();
+                    }
+                }
+            }
+        }
+
+        Result::Ok(Tokens::EOF)
+    }
+
+    // NEGATIVE NUMBERS TO BE HANDLED BY PARSER
+    fn scan_number(&mut self) -> Result<i32> {
+        // safe to unwrap twice since we know we putback
+        let c = self.consume().unwrap().unwrap();
+        if c == '0' {
+            if let Some(c) = self.consume()? {
+                if c == 'x' {
+                    self.scan_base(16)
+                } else {
+                    self.putback(c);
+                    self.scan_base(8)
+                }
+            } else {
+                self.putback(c);
+                self.scan_base(10)
+            }
+        } else {
+            self.putback(c);
+            self.scan_base(10)
+        }
+    }
+
+    fn scan_base(&mut self, base: i32) -> Result<i32> {
+        let mut num = 0_i32;
+
+        while let Some(c) = self.consume()? {
+            if let Some(d) = c.to_digit(10) {
+                num += d as i32;
+            } else {
+                self.putback(c);
+                break;
+            }
+        }
+
+        Ok(num)
+    }
+
+    fn match_keyword(&mut self) -> Result<Tokens> {
+        let ident = self.scan_ident()?;
+        match ident.as_str() {
+            "fn" => Ok(Tokens::FN),
+            "let" => Ok(Tokens::LET),
+            "if" => Ok(Tokens::IF),
+            "else" => Ok(Tokens::ELSE),
+            "for" => Ok(Tokens::FOR),
+            "struct" => Ok(Tokens::STRUCT),
+            "union" => Ok(Tokens::UNION),
+            "u0" => Ok(Tokens::U0),
+            "i8" => Ok(Tokens::I8),
+            "i16" => Ok(Tokens::I16),
+            "i32" => Ok(Tokens::I32),
+            _ => Ok(Tokens::IDENT(ident)),
+        }
+    }
+
+    fn scan_ident(&mut self) -> Result<String> {
+        let mut ident = String::new();
+
+        while let Some(c) = self.consume()? {
+            if !c.is_alphanumeric() {
+                self.putback(c);
+                break;
+            }
+
+            ident.push(c);
+        }
+
+        Result::Ok(ident)
+    }
+
+    fn scan_string(&mut self) -> Result<String> {
+        let mut str = String::new();
+        
+        while let Some(c) = self.consume()? {
+            if c == '\"' {
+                return Result::Ok(str);
+            }
+
+            if c == '\\' {
+                if let Some(escape_chr) = self.consume()? {
+                    match escape_chr {
+                        'n' => str.push('\n'),
+                        'r' => str.push('\r'),
+                        't' => str.push('\t'),
+                        '"' => str.push('\"'),
+                        _ => {
+                            self.putback(escape_chr);
+                            str.push('\\');
+                        }
+                    }
+                    continue;
+                }
+            } 
+
+            str.push(c);
+        }
+
+        Result::Err(LexerError::Msg("Unterminated String".to_string()))
+    }
+}
